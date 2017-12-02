32908c34626f750fd7ebcb4d99c925c8023ec77d
Naji <naji.sawas.contact@gmail.com>
2017-12-01 17:00:44 -0800

    express with react

--- server.js
+++ server.js
@@ -1,53 +1,36 @@
-const express = require('express');
-const session  = require('express-session');
-const bodyParser = require('body-parser');
-const routes = require('./controllers/recipeController.js');
-const jquery = require('jquery');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const passport = require('passport');
+var express = require(‘express’);
+var mongoose = require(‘mongoose’);
+var bodyParser = require(‘body-parser’);
 
-require('./config/passport')(passport); // pass passport for configuration
+var app = express();
+var router = express.Router();
 
+var port = process.env.API_PORT || 3000;
 
-const port = process.env.PORT || 3000;
-
-const app = express();
-
-
-app.use(express.static('public'));
-app.use(cookieParser()); // read cookies (needed for auth)
-// required for passport
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var options = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'lunchy_db'
-};
+//To prevent errors from Cross Origin Resource Sharing, we will set
+//our headers to allow CORS with middleware like so:
+app.use(function(req, res, next) {
+ res.setHeader(‘Access-Control-Allow-Origin’, ‘*’);
+ res.setHeader(‘Access-Control-Allow-Credentials’, ‘true’);
+ res.setHeader(‘Access-Control-Allow-Methods’, ‘GET,HEAD,OPTIONS,POST,PUT,DELETE’);
+ res.setHeader(‘Access-Control-Allow-Headers’, ‘Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers’);
 
-var sessionStore = new MySQLStore(options);
-
-
-app.use(session({
-    secret: '1hg9280gej32SGSlhfxsbnv482',
-    resave: false,
-    store: sessionStore,
-    saveUninitialized: false
- } )); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
+//remove cacheing
+ res.setHeader(‘Cache-Control’, ‘no-cache’);
+ next();
+});
 
 
+router.get(‘/’, function(req, res) {
+ res.json({ message: ‘API Initialized!’});
+});
+//Use our router configuration when we call /api
+app.use(‘/api’, router);
 
-app.use(morgan('dev')); // log every request to the console
 
-
-app.use('/', routes);
-
-
-app.listen(port);
-console.log('The app is running on port ' + port);
+app.listen(port, function() {
+ console.log(`api running on port ${port}`);
+});
