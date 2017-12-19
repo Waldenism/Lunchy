51db8ac92e415cba8fe6a8d96575adfebe52eb47
Chrisplaysharp <cwalden@berkeley.edu>
2017-12-19 15:53:42 -0800

    component dirs

--- client/src/index.js
+++ client/src/index.js
@@ -1,50 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-
-class Signup extends React.Component {
-    render() {
-        return(
-            <div>
-                <h1> Sign Up! </h1>
-                <form action="/signup" method="post">
-                    <div class="form-group">
-                        <label>Username</label>
-                        <input type="text" class="form-control" name="username"></input>
-                    </div>
-                    <div class="form-group">
-                        <label>Password</label>
-                        <input type="password" class="form-control" name="password"></input>
-                    </div>
-
-                    <button type="submit" class="btn btn-warning btn-lg">Signup</button>
-                </form>
-            </div>
-        )
-    }
-};
-
-class Login extends React.Component {
-    render() {
-        return(
-            <div>
-                <h1> Log In! </h1>
-                <form action="/login" method="post">
-                    <div class="form-group">
-                        <label>Username</label>
-                        <input type="text" class="form-control" name="username"></input>
-                    </div>
-                    <div class="form-group">
-                        <label>Password</label>
-                        <input type="password" class="form-control" name="password"></input>
-                    </div>
-
-                    <button type="submit" class="btn btn-warning btn-lg">Login</button>
-                </form>
-            </div>
-        )
-    }
-}
-
-ReactDOM.render(<Signup />, document.getElementById('signup'));
-ReactDOM.render(<Login />, document.getElementById('login'));
+ReactDOM.render(<App />, document.getElementById('root'));
